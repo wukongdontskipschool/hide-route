0ed83e1e3db9d047aa5d7d935e9518d20fa07d7c
zhouzijun <coonghuaa@163.com>
2025-07-04 14:38:25 +0800

    兼容原本中间件

--- src/HideRoute.php
+++ src/HideRoute.php
@@ -1,10 +1,25 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('hide-route')->group(function () {
-    Route::get('/test', function () {
-        return '<h1>test</h1>';
+Route::middleware([
+        ...getMiddleware('web'),
+    ])
+    ->prefix('hide-route')
+    ->group(function () {
+        Route::get('test', function () {
+            return '<h1>test</h1>';
+        });
     });
-});
+
+
+/**
+ * 获取默认中间件
+ * @param string $group web|api
+ * @return array
+ */
+function getMiddleware($group = 'web')
+{
+    $middleware = new \Illuminate\Foundation\Configuration\Middleware();
+    return $middleware->getMiddlewareGroups()[$group];
+}
